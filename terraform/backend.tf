a5e4e7e814855946a86b17faff74b4b346fdde25
Joe K <jkrilov@gmail.com>
2023-06-01 08:20:16 -0400

    fix: provider versions

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,9 +1,13 @@
 terraform {
-  required_version = ">= 1.3.0, <2.0.0"
+  required_version = ">= 1.4.0, <2.0.0"
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "3.37.0"
+      version = "3.58.0"
+    }
+    random = {
+      source = "hashicorp/random"
+      version = "3.5.1"
     }
   }
   backend "azurerm" {
