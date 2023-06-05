a59ca919a62c02013c0d29669ca1caf40ce99362
Joe K <jkrilov@gmail.com>
2023-06-05 10:53:29 -0400

    create game

--- app/backend/main.py
+++ app/backend/main.py
@@ -1,8 +1,20 @@
+import uuid
 from fastapi import FastAPI
 
 app = FastAPI()
 
+class PokerGame(BaseModel):
+    id: uuid.UUID
+    name: str
+    players: list
+    status: str
 
 @app.get("/")
 def read_root():
     return {"ACA-Poker": "Hello World!"}
+
+@app.post("/game")
+async def create_game(game: PokerGame):
+    gameID = uuid.uuid4()
+    game.id = game_id
+    return {"game_id": str(game_id), "game": game}
